69ddad5ce27313c0216f7b674a215ff5fda8e49c
Jasmine Youngharry <jazzyyoungharr@gmail.com>
2026-05-08 12:42:16 -0600

    Set up OMDb API preparation and JavaScript event listener

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select DOM Elements
+const searchBtn = document.getElementById("search-btn");
+const movieInput = document.getElementById("movie-input");
+
+// Test Button Click
+searchBtn.addEventListener("click", () => {
+    console.log("Search button clicked");
+});
